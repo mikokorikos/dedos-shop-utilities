dd28972844c974f274900b2fc1a981dbd92aaad2
Alan Escarcha <alanescarcha11@gmail.com>
2025-09-03 16:30:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // ===================================================================
-// You can delete all these files and upload your project or bot.
+// English: You can delete all these files and upload your project or bot.
+// Español: Puede eliminar todos estos archivos y cargar tu propio proyecto o bot.
 // ===================================================================
+
+// This bot is just an example. When you type "ping" in a Discord channel, the bot responds with the message "pong".
+// Este bot es solo un ejemplo. Cuando escribes "ping" en un canal de Discord, el bot responde con el mensaje "pong".
 
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -16,4 +20,6 @@ client.on("message", msg => {
 })
 
 // Change your discord bot token here
+// Cambia aquí tu token de Discord Bot
 client.login('Your token here')
+
